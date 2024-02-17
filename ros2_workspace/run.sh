ae28e94da03b3e6ca37bdcdf11bc08c3d6a28aa6
amw <alexander@anak.dk>
2024-02-17 17:21:55 +0100

    user permissions

--- ros2_workspace/run.sh
+++ ros2_workspace/run.sh
@@ -1,18 +1,19 @@
-sudo chmod a+rw /dev/input/event7
 docker container prune
 
 docker run --network host \
     --name ros2 \
-    --user 1000:1000 \
-    --volume /home/alexander/Documents/simplebot2/ros2_workspace/:/home/alexander/Documents/simplebot2/ros2_workspace/ \
+    --user 1001:alexander \
+    --group-add=dialout \
+    --group-add=messagebus \
+    --volume /home/alexander/simplebot2/ros2_workspace/:/home/alexander/simplebot2/ros2_workspace/ \
     --env="DISPLAY" \
     --env="QT_X11_NO_MITSHM=1" \
     --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
     --volume="/etc/localtime:/etc/localtime:ro" \
     --volume="/etc/timezone:/etc/timezone:ro" \
+    --device=/dev/input/event2 \
     --device=/dev/ttyACM0 \
-    --device=/dev/input/event7 \
     -it \
-    ros2 bash
+    ros2 bash 
 
-
+#
