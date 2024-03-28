d4e09f4ef9dca008c0f08976f03c68fd4db30b3f
amw <alexander@anak.dk>
2024-03-28 13:49:05 +0100

    source lidar

--- docker_workspace/ros-entrypoint.sh
+++ docker_workspace/ros-entrypoint.sh
@@ -3,4 +3,5 @@
 source /opt/ros/humble/install/setup.bash
 source /opt/ros/humble/setup.bash
 source /home/alexander/simplebot2/ros2_workspace/install/setup.bash
+source /home/alexander/simplebot2/ros2_workspace/src/sllidar_ros2/scripts/create_udev_rules.sh
 exec "$@"
