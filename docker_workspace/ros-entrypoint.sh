104f30ecdbe372e4a0ba5a1a159d6ef583f5815e
alexander <alexander@anak.dk>
2025-03-22 08:29:27 +0100

    init

--- docker_workspace/ros-entrypoint.sh
+++ docker_workspace/ros-entrypoint.sh
@@ -4,4 +4,5 @@ source /opt/ros/jazzy/setup.bash
 source /home/alexander/simplebot2/ros2_workspace/install/setup.bash
 export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
 source /home/alexander/simplebot2/ros2_workspace/src/sllidar_ros2/scripts/create_udev_rules.sh
+source /home/alexander/venv/bin/activate
 exec "$@"
